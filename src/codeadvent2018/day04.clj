83cb7031987a058e22f91fd1ea50cdd3bb8ce33d
deepbit <deepbit@deepbits-MacBook-Pro.local>
2018-12-07 20:39:26 +0000

    day4

--- src/codeadvent2018/day04.clj
+++ src/codeadvent2018/day04.clj
@@ -1,8 +1,6 @@
 (ns codeadvent2018.day04)
 
-
 (def data ["[1518-09-28 00:56] wakes up" "[1518-10-15 00:05] falls asleep" "[1518-02-15 00:58] wakes up" "[1518-08-26 00:51] wakes up" "[1518-03-23 00:32] wakes up" "[1518-05-04 23:56] Guard #523 begins shift" "[1518-07-28 00:55] wakes up" "[1518-02-08 00:49] wakes up" "[1518-04-20 00:02] Guard #2017 begins shift" "[1518-09-12 00:57] wakes up" "[1518-06-11 00:34] wakes up" "[1518-06-08 00:20] wakes up" "[1518-01-29 00:53] wakes up" "[1518-09-23 00:03] falls asleep" "[1518-02-11 00:19] falls asleep" "[1518-05-12 23:57] Guard #151 begins shift" "[1518-07-24 00:47] wakes up" "[1518-07-21 00:03] falls asleep" "[1518-05-19 00:54] wakes up" "[1518-08-21 00:52] wakes up" "[1518-10-10 00:52] wakes up" "[1518-02-02 00:14] wakes up" "[1518-10-15 00:52] wakes up" "[1518-03-13 23:46] Guard #2081 begins shift" "[1518-08-07 00:26] falls asleep" "[1518-08-13 00:00] Guard #349 begins shift" "[1518-10-17 00:22] falls asleep" "[1518-03-10 00:14] falls asleep" "[1518-09-30 00:48] wakes up" "[1518-07-01 23:57] Guard #3469 begins shift" "[1518-04-09 23:57] Guard #3413 begins shift" "[1518-04-28 00:35] falls asleep" "[1518-06-08 00:49] wakes up"])
-
 
 (defn parse-record [r]
   (let [[year day month hour minute info]
@@ -12,24 +10,33 @@
      :time (mapv #(Integer/parseInt %) [hour minute])
      :wakeup (= info "up")
      :asleep (= info "asleep")
-     :id (if (= (first info) \#) info)}))
+     :id (if (= (first info) \#) (Integer/parseInt (subs info 1)))}))
 
 
-(clojure.pprint/pprint
- (partition-by :id (sort-data data)))
+(defn agg-sleep-times [inp]
+  (loop [input inp acum {} id (:id (first input))]
+    (if (empty? input)
+      acum
+      (let [r (parse-record (first input))
+            new-input (rest input)
+            new-id (or (get r :id) id)]
+        (recur new-input
+               (assoc-in acum
+                         [new-id (:time r)]
+                         (inc (get-in acum [new-id (:time r)] 0)))
+               new-id)))))
+
+(defn part1 [input]
+  (let [agg-times (agg-sleep-times input)
+        [id [[_ min] _]] (apply max-key (comp second second)
+                                (mapcat (fn [[id times]]
+                                          (mapv #(identity [id %]) times))
+                                        agg-times))]
+    (println agg-times)
+    (println [id min])
+    (* id min)
+    ))
+
 
 (defn run [input]
-  (println "-"))
-
-(let [dat (map parse-record (sort data))]
-  (reduce
-   (fn [prev new]
-
-     )
-   {}
-   dat))
-
-
-(reduce
- {}
- (sort-data ))
+  (println (part1 input)))
